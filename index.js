979a329db3f751500ed828593f3e29fbd5435d5f
samtapes <samtapes48@gmail.com>
2020-08-17 15:15:58 -0300

    Push with process.env.PORT

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3333;
 
 const flood = require("./flood");
 
@@ -15,6 +16,6 @@ app.post("/login", async (req,res) => {
 });
 
 
-app.listen(3333, () => {
+app.listen(PORT, () => {
     console.log("Server is running!")
 });
