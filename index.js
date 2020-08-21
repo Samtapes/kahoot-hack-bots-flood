5e290fdfe76c5f9bebe12798d4e7e7456a813591
samtapes <samtapes48@gmail.com>
2020-08-21 10:05:09 -0300

    changed front and back for server02 and 03

--- index.js
+++ index.js
@@ -15,35 +15,31 @@ app.use(express.json());
 
 
 // CORS enabling the server01 acess all the servers server
-app.use(function(req, res, next){
-    var origins = ['kahoot-flood-server01.herokuapp.com', 'kahoot-flood-server01.herokuapp.com/', 'https://kahoot-flood-server01.herokuapp.com/', 'https://kahoot-flood-server01.herokuapp.com'];
-    for(var i=0;i<origins.length;i++){
-        var origin = origins[i];
-        if(req.headers.origin.indexOf(origin) > -1){ 
-             res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-             return;
-        }
-        // else, tough cookies. 
+app.use(function(req, res, next) {
+    var allowedOrigins = ['https://kahoot-flood-server01.herokuapp.com/'];
+    var origin = req.headers.origin;
+    if(allowedOrigins.indexOf(origin) > -1){
+        res.setHeader('Access-Control-Allow-Origin', origin);
     }
-});
+    //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', true);
+    return next();
+  });
 
 
-
-// Rendering frontend
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname + '/index.html'))
-});
 
 
 
 // Sending data to the frontend to check if the server are full and the players name
-app.get('/login', async (req,res) => {
+app.get('/', async (req,res) => {
     return res.json(bots);
 });
 
 
 // Creating bots
-app.post("/login", async (req,res) => {
+app.post("/", async (req,res) => {
     const data = req.body;
     
     const resposta = flood(data);
